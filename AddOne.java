df353d63379bb016d2a2fddf45d72fd27f544152
Georgem821 <78710322+Georgem821@users.noreply.github.com>
2021-02-08 10:39:58 -0500

    Add files via upload

---
+++ AddOne.java
@@ -0,0 +1,12 @@
+
+public class AddOne {
+	public static void main(String[] args) {
+		int integer = 10;
+		
+		System.out.println("Enter a number: " + integer + 1);
+		int sum = integer;
+		
+		System.out.println("The sum is: " + sum);
+	}
+
+}
---
+++ Credentials.java
@@ -0,0 +1,9 @@
+
+public class Credentials {
+	public static void main(String[] args) {
+	System.out.println("Credentials: George Moller, gmoller@nyit.edu, 1276285");
+	}
+	
+	
+
+}
